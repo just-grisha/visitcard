5f61379ac5008903c92689d366ebd86b0bd8c976
Grisha <just.grishaayf@gmail.com>
2023-04-14 23:04:02 +0300

    RegistrationFrom added

---
+++ src/Components/Registration/RegistrationComponent.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Header from './Header'
+import RegistrationForm from './RegistrationForm'
+
+export default function RegistrationComponent() {
+  return (
+    <>
+    <Header/>
+    <RegistrationForm/>
+    </>
+  )
+}
